75e7ee8cc7d2476c32b9d13b64babfc7c80d0ecc
nmsn <136696700@qq.com>
2026-06-04 21:23:53 +0800

    chore(web): disable vite error overlay in dev server

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 5173,
+    hmr: {
+      overlay: false,
+    },
   },
   css: {
     postcss: './postcss.config.js',
